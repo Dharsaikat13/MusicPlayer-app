41378141b0a4a2092773c8c07830bf900fa8cafe
SAIKAT <37828689+Dharsaikat13@users.noreply.github.com>
2021-05-06 01:28:39 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,54 @@
+let allMusic=[
+
+
+{
+
+    name:"Rabbta",
+    artist: "Arijit Singh",
+    img:"music-1",
+    src:"music-1",
+    
+},
+{
+
+    name:"Tum hi anna",
+    artist: "Arijit Singh",
+    img:"music-2",
+    src:"music-2",
+    
+},
+{
+
+    name:"Sukoon mila",
+    artist: "Arijit Singh",
+    img:"music-3",
+    src:"music-3",
+    
+},
+{
+
+    name:"Main tera boyfriend",
+    artist: "Arijit Singh",
+    img:"music-4",
+    src:"music-4",
+    
+},
+{
+
+    name:"Raat Bhar",
+    artist: "Arijit Singh",
+    img:"music-5",
+    src:"music-5",
+    
+},
+{
+
+    name:"Hawayana",
+    artist: "Arijit Singh",
+    img:"music-6",
+    src:"music-6",
+    
+}
+
+
+]
