2bde988d71d82f3ef50776a155cad89b34af95e2
SAIKAT <37828689+Dharsaikat13@users.noreply.github.com>
2021-05-06 14:47:12 +0530

    Add files via upload

--- music-list.js
+++ music-list.js
@@ -48,7 +48,252 @@ let allMusic=[
     img:"music-6",
     src:"music-6",
     
+},
+{
+
+    name:"Shape Of you",
+    artist: "Ed Sheeran",
+    img:"defult",
+    src:"music-7",
+    
+},
+{
+
+    name:"Dil Diyan Gallan",
+    artist: "Atif Aslam",
+    img:"defult",
+    src:"music-8",
+    
+},
+{
+
+    name:"Bom Diggy",
+    artist: "Zack Knight",
+    img:"defult",
+    src:"music-9",
+    
+},
+{
+
+    name:"O Saathi",
+    artist: "Atif Aslam",
+    img:"defult",
+    src:"music-10",
+    
+},
+{
+
+    name:"Nachle na",
+    artist: "Guru Randhwa",
+    img:"defult",
+    src:"music-11",
+    
+},
+{
+
+    name:"The jawaani Song",
+    artist: "Vishal Dadlani & Palak Munchal",
+    img:"defult",
+    src:"music-12",
+    
+},{
+
+    name:"Sweetheart",
+    artist: "Dev Negi",
+    img:"defult",
+    src:"music-13",
+    
+},
+{
+
+    name:"The Hook Up song",
+    artist: "Neha kakkar",
+    img:"defult",
+    src:"music-14",
+    
+},
+{
+
+    name:"Mile Ho Tum Humko",
+    artist: "Neha kakkar & Tony kakkkar",
+    img:"defult",
+    src:"music-15",
+    
+},
+{
+
+    name:"Leja Re",
+    artist: "Dhvani Bhanushali",
+    img:"defult",
+    src:"music-16",
+    
+},
+{
+
+    name:"Kuch Kuch",
+    artist: "Tony kakkar",
+    img:"defult",
+    src:"music-17",
+    
+},
+{
+
+    name:"Kalank Title Song",
+    artist: "Arijit Singh",
+    img:"defult",
+    src:"music-18",
+    
+},
+{
+
+    name:"First Class",
+    artist: "Arijit Singh",
+    img:"defult",
+    src:"music-19",
+    
+},
+{
+
+    name:"Bekhayali Title Song",
+    artist: "Arijit Singh",
+    img:"defult",
+    src:"music-20",
+    
+},
+{
+
+    name:"kaise Hua",
+    artist: "Vishal Mishra",
+    img:"defult",
+    src:"music-21",
+    
+},
+{
+
+    name:"pehala Pyaar",
+    artist: "Armaan Malik",
+    img:"defult",
+    src:"music-22",
+    
+},
+{
+
+    name:"Tera Ban Jaunga",
+    artist: "Tulsi Kumar",
+    img:"defult",
+    src:"music-23",
+    
+},
+{
+
+    name:"Tujhe Kitna Chaya Aur Hum",
+    artist: "Arijit Singh",
+    img:"defult",
+    src:"music-24",
+    
+},
+{
+
+    name:"Mon Majhi Re",
+    artist: "Arijit Singh",
+    img:"defult",
+    src:"music-25",
+    
+},
+{
+
+    name:"Oh Tomari Cholar Potha (Remake)",
+    artist: "Arijit Singh",
+    img:"defult",
+    src:"music-26",
+    
+},
+{
+
+    name:"Romantic Bollywood mashup",
+    artist: "Music Libary",
+    img:"defult",
+    src:"music-27",
+    
+},
+{
+
+    name:"Main Rahoon Ya Na Rahhon",
+    artist: "Armaan Mallick",
+    img:"defult",
+    src:"music-28",
+    
+},
+{
+
+    name:"Baatein YE kabhi na",
+    artist: "Arijit Singh",
+    img:"defult",
+    src:"music-29",
+    
+},
+{
+
+    name:"Oviman",
+    artist: "Unknown",
+    img:"defult",
+    src:"music-43",
+    
+},
+{
+
+    name:"Khairiyat",
+    artist: "Arijit Singh",
+    img:"defult",
+    src:"music-37",
+    
+},
+{
+
+    name:"Fihall",
+    artist: "B Praak & Akshay Kumar",
+    img:"defult",
+    src:"music-40",
+    
+},
+{
+
+    name:"Bollywoood Party Mashup",
+    artist: "Unknown",
+    img:"defult",
+    src:"music-38",
+    
+},
+{
+
+    name:"Love Mashup",
+    artist: "Unknown",
+    img:"defult",
+    src:"music-34",
+    
+},
+{
+
+    name:"Lut Gaye",
+    artist: "Jubin Nautiyal",
+    img:"defult",
+    src:"music-46",
+    
+},
+{
+
+    name:"Aankh Marey",
+    artist: "Mika Singh & Neha Kakkar",
+    img:"defult",
+    src:"music-32",
+    
 }
+
+
+
+
+
+
 
 
 ]
